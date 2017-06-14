ea36aa796480524b787cfe23fcf336e545dc362b
markinb66 <markinb66@gmail.com>
2017-06-14 05:24:11 -0800

    Create nitrogen_d850.mk

--- nitrogen_d850.mk
+++ nitrogen_d850.mk
@@ -17,15 +17,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common Nitrogen stuff.
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
 # Inherit from d850 device
 $(call inherit-product, device/lge/d850/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d850
-PRODUCT_NAME := aosp_d850
+PRODUCT_NAME := nitrogen_d850
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D850
 PRODUCT_MANUFACTURER := LGE
